de32e232f78dd782a113c55b59e99ee7c267a15d
LiangYong <liangdongxing.cn@outlook.com>
2023-06-07 16:05:41 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 import {ref} from 'vue'
 
 const url = ref('https://dongxing.fun:31204')
+
 </script>
 
 <template>
-  <iframe src="{{url}}"></iframe>
+  <iframe :src="url"></iframe>
 </template>
 
 <style scoped>

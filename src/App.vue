6ebf0d774864eba1073673cef4241a4b65e1573d
LiangYong <liangdongxing.cn@outlook.com>
2023-06-07 10:07:08 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <iframe src="http://www.dongxing.fun:31204"></iframe>
+  </main>
+</template>
+
+<style scoped>
+iframe {
+  width: 100vh;
+  height: 100vh;
+  border: none;
+}
+</style>
